614a6511f3dfb47d61e52664bf8b62cb9f0b9f15
Shashank Pandey <72259097+shashank088@users.noreply.github.com>
2022-10-29 00:02:59 +0530

    Create nCrnCp.cpp

---
+++ nCrnCp.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int nCrModpDP(int n, int r, int p)
+{
+
+  int C[r+1];
+  memset(C, 0, sizeof(C));
+
+  C[0] = 1; 
+  for (int i = 1; i <= n; i++)
+  {
+
+    for (int j = min(i, r); j > 0; j--)
+
+      C[j] = (C[j] + C[j-1])%p;
+  }
+  return C[r];
+}
+
+int nCrModpLucas(int n, int r, int p)
+{
+  if (r==0)
+    return 1;
+
+  int ni = n%p, ri = r%p;
+
+  return (nCrModpLucas(n/p, r/p, p) * // Last digits of n and r
+      nCrModpDP(ni, ri, p)) % p; // Remaining digits
+}
+
+
+int main()
+{
+  int n = 1000, r = 900, p = 13;
+  cout << "Value of nCr % p is " << nCrModpLucas(n, r, p);
+  return 0;
+}
